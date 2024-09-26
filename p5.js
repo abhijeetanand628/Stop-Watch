75d37c7aae1a4e059823bf14c381585dbcd79a5b
Abhijeet Anand <abhijeetanand628@gmail.com>
2024-09-26 20:49:55 +0530

    yes

---
+++ p5.js
@@ -0,0 +1,30 @@
+const clock = document.querySelector(".clock")
+
+let sec = 0, min = 0,hr = 0;
+let intervalId;
+
+
+const start = () => {
+    intervalId = setInterval (() => {
+        if(sec < 59) {
+            sec++;
+        } else if (min >= 59) {
+            hr++;
+            min = 0;
+        } else {
+            sec = 0;
+            min++;
+        }
+
+        const seconds = sec.toString().padStart(2,"0"); 
+        const minutes = min.toString().padStart(2,"0"); // These are used to keep two zeroes in the clock
+        const hours = hr.toString().padStart(2,"0");
+
+        clock.innerText = `${hours}:${minutes}:${seconds}`;
+    }, 1000)
+
+}
+
+const stop = () => {
+    clearInterval(intervalId);
+}
